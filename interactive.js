c945e70236414c943b0978b4000d05a08ba1e53d
fogcity89 <kevinmtow@gmail.com>
2015-12-23 13:42:03 -0800

    Update interactive.js

--- interactive.js
+++ interactive.js
@@ -1,3 +1,14 @@
+/*
+A problem that I am working on,
+In an array of objects some of the values contain additional objects. I would like to know how to access these inner objects..
+
+On my github I would like to know how to push the only 'bookmark' values.
+I wrote a function that pushes the new values on line 37. 
+
+Lets say I just want to only call the  'time' or bookmark.id values. 
+My function code works for calling properties and values, I would like to dig deeper in objects. 
+*/
+
 function badboys() {
 	var newReleases = [
 		{
